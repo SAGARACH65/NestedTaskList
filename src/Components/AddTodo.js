66b81de2c036b92d523bab74b2afc5863e6ea7f9
Sagar Acharya <sagarach65@gmail.com>
2018-07-01 21:45:17 +0545

    Added AddTodo functionality

--- src/Components/AddTodo.js
+++ src/Components/AddTodo.js
@@ -2,12 +2,10 @@ import React, {Component} from 'react';
 
 import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
-import Icon from '@material-ui/core/Icon';
-import DeleteIcon from '@material-ui/icons/Delete';
-import NavigationIcon from '@material-ui/icons/Navigation';
+
 
 import {store} from '../Store'
-import {addTodo, removeTodo, setVisibility, Visibility} from "../Actions";
+import {addTodo} from "../Actions";
 
 class AddTodo extends Component {
     render() {
